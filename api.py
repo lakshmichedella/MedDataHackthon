28c1b3d1ae74e58d7e60e69ae3507865b348b7e2
Praju C <lpchedel@uwaterloo.ca>
2026-03-08 11:51:57 -0230

    Added ML stuff

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify, render_template
+import xgboost as xgb
+import pandas as pd
 app = Flask(__name__)
 
+loaded_model = xgb.XGBClassifier()
+loaded_model.load_model('model')
 
 @app.route('/')
 def index():
@@ -13,15 +17,31 @@ def submit():
     if request.method == 'POST':
 
         print(request)
+        floats = ["oldpeak", "ca", "thal"]
+
+        
         # Get the data from the form using the 'name' attribute from the HTML input field
-        user_input_data = "HElLop"
+        data = request.form
+
+        data = data.to_dict(flat=False)
+
+        feature_names = data.keys()
+        feature_data = {}
+        
+        for key in feature_names:
+            if key in floats:
+                feature_data[key] = [float(data[key][0])]
+            else:
+                feature_data[key] = [int(data[key][0])]
+
+        df = pd.DataFrame(feature_data)
         
         # You can now use 'user_input_data' in your Python code (e.g., print it, save to a database, etc.)
-        print("Data received from form:", user_input_data)
+        predicted = loaded_model.predict(df)
         
         # Return a confirmation message or another template
-        return render_template('index.html', answer=user_input_data)
+        return render_template('index.html', answer=predicted[0])
     
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
